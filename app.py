91e72f1d7d99d96299f25ca82f14ad7b295f8cb4
wataru sasaki <make.it.sskwtr.40@gmail.com>
2020-11-27 20:43:18 +0900

    confirm

---
+++ app.py
@@ -0,0 +1,23 @@
+# Flaskからimportしてflaskを使えるようにする。
+from flask import Flask, render_template, request, redirect
+import sqlite3
+import random
+
+# appっていう名前でFlaskアプリを作っていくよーみたいな
+app = Flask(__name__)
+
+# 秘密鍵
+app.secret_key = "prog"
+
+@app.route("/")
+def template():
+    return render_template("top.html")
+
+@app.errorhandler(404)
+def notfound(code):
+    return "404ページだよ。ごめんね"
+
+
+if __name__ == "__main__":
+    # Flaskが持っている開発用サーバーを実行します。
+    app.run(debug=True)
