b96d5f65e3fdff55f299cbebe2f71da383379553
hrd3a2577 <hrdea2577@gmail.com>
2020-12-03 20:54:07 +0900

    python menu deleta

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ def template():
 def menu():
     return render_template("menu.html")
 
-@app.route("/menu2")
-def menu2():
-    return render_template("menu2.html")
-
-@app.route("/menu3")
-def menu3():
-    return render_template("menu3.html")
-
 @app.errorhandler(404)
 def notfound(code):
     return "404ページだよ。ごめんね"
